ec2030118e49476f01edb5ba6f23701d263f5ea3
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-05 15:10:45 +0300

    Some data types added

---
+++ dataTypes.py
@@ -0,0 +1,38 @@
+# ======== Data Types in Python ============
+
+# ---> type of data
+name = 'bro'
+print(type(name))
+
+# ----> STRING
+
+first_name = 'Tauhid'
+last_name = 'Hasan'
+full_name = first_name + " " + last_name
+print(full_name)
+
+# ----> INT (it only store whole number - can not store a decimal number)
+
+age = 23
+age += 1
+print(age)
+# we can only concat same data type
+print("Your age is " + str(age))
+print(type(age))
+
+# ----> FLOAT (floating point number - a decimal number)
+
+height = 230.50
+print(height)
+print("Your height is saaaaaaaa " + str(height))
+print(type(height))
+
+
+
+
+
+
+
+
+
+
