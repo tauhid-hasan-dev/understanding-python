7e8770da2690b1614d6223c97acbf391f63d41d3
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-05 16:49:16 +0300

    multiple assignment explained

---
+++ multipleAssignment.py
@@ -0,0 +1,31 @@
+# Multiple Assignment = allows us to assign multiple variables at the same line of code.
+
+age = 30
+name = 'Tauhid Hasan'
+isDeveloper = True
+
+print(age)
+print(name)
+print(isDeveloper)
+
+# ----> we can write this in one line using multiple assignment technique
+id, student, isStudent = 56, "Asik hasan", True
+
+print(id)
+print(student)
+print(isStudent)
+
+# ----> If the value of multiple variable is same we can write them like this
+
+hasan = 40
+mehedi = 40
+muaz = 40
+mohsin = 40
+
+
+hasan = mehedi = muaz = mohsin = 40
+
+print(hasan)
+print(mehedi)
+print(muaz)
+print(mohsin)
