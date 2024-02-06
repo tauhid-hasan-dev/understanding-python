4e4886312c6ff7f8a08c3c74978b3e3dead5c603
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-06 15:02:18 +0300

    understanding type casting

---
+++ type_casting.py
@@ -0,0 +1,19 @@
+# -> TYPE_CASTING = convert the data type of value to another data type.
+
+x = 1
+y = 3.0
+z = '40'
+
+y = int(y)
+print(y)
+print(z)
+
+# ---> we generally use type casting when we want to concat
+print("X is " + str(x))
+
+
+
+
+
+
+
