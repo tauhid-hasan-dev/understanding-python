e74d598a2a749ee5c937ec135b5b69d40c8b9fe6
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-06 15:19:25 +0300

    understanding accepting user input in python

---
+++ input.py
@@ -0,0 +1,12 @@
+name = input("What is your name :")
+age = int(input("How old are you? : "))
+height = float(input("How tall are you"))
+
+print("Hello " + name)
+print("I see you are " + str(age) + " years old")
+print("Your height is " + str(height))
+
+
+
+
+
