d6c513596c281e4b9eec2fee41afbcd01da6164e
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-06 16:57:01 +0300

    understanding slicing in string

---
+++ functions_of_number.py
@@ -0,0 +1,33 @@
+# Math functions (functions related to the Number)
+import math
+
+pi = 3.5
+
+# Round will round the number to the nearest whole integer
+print(round(pi))
+
+# Ceil will round to the highest whole integer
+print(math.ceil(pi))
+
+# Floor will round to the lowest whole integer
+print(math.floor(pi))
+
+# ABS: it will give us the absolute value of a number, means how far the value from 0;
+# if the value of pi is a negative number the output will be a positive value
+print(abs(pi))
+
+# pow : will rase a base number to a power
+print(pow(pi, 2))
+
+# Square root
+print(math.sqrt(64))
+
+x = 1
+y = 2
+z = 3
+
+print(min(x, y, z))
+print(max(x, y, z))
+
+
+
---
+++ string_slicing.py
@@ -0,0 +1,26 @@
+# Slicing: create a substring by extracting elements from another string
+# There are two ways to slice: indexing[] or slice()
+
+# indexing[] --> [start: stop : step]
+
+name = "Tauhid Hasan"
+
+first_name = name[:6]       # [start:6]
+last_name = name[7:]        # [7:end]
+funky_name = name[::2]      # [start:end:2] (it will start o index and step up by 2)
+reversed_name = name[::-1]  # [start:end:-1]
+
+
+print(first_name)
+print(last_name)
+print(funky_name)
+print(reversed_name)
+
+
+# slice(start, end)
+
+website1 = "https://google.com"
+
+slice = slice(8, -4)
+
+print(website1[slice])
