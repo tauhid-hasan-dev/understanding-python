9b16d211bb609f54dcc406cc6bf64d2398ec8535
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-08 16:05:15 +0300

    list is updated

---
+++ 2d_list.py
@@ -0,0 +1,12 @@
+# 2d lists = a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["steak", "asian food", "soup"]
+desserts = ["cake", "ice cream"]
+
+food = [drinks, dinner, desserts]
+
+# output will be 'soda'
+
+print(food)
+print(food[0][1])
