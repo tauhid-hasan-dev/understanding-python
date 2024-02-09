3d6670cca48773b692df96773b837664dc9b7c38
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-09 16:43:02 +0300

    understanding function in python

---
+++ functions.py
@@ -0,0 +1,11 @@
+# function = a block of code which is executed only when it is called
+
+def hello(first_name, last_name, age):
+    print("Hello, this is " + first_name + " " + last_name)
+    print("And my age is " + age)
+
+
+last_name = "Hasan"
+first_name = "Tauhid"
+age = 23
+hello(first_name, last_name, str(age))
