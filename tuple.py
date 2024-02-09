784f1869e2952800d673c47448d11fdbcf1c9bf5
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-09 09:12:13 +0300

    understanding tuple in python

---
+++ tuple.py
@@ -0,0 +1,14 @@
+# tuple = collection which is ordered and unchangeable used to group together related data
+
+student  = ("Tauhid", 28, "male")
+
+print(student.count("Tauhid"))
+print(student.index("male"))
+
+for x in student:
+    print(x)
+
+if 22 in student:
+    print('there is 28 in student')
+else:
+    print("Not found")
