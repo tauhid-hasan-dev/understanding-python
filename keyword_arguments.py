ef52fc9d8c44f929ffc1d1b3a89fb2a57f7253d9
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-12 09:18:01 +0300

    understanding keyword arguments in function

---
+++ keyword_arguments.py
@@ -0,0 +1,11 @@
+# keyword arguments = arguments preceded by an identifier when we pass them to a function
+#                     The order of the arguments does not matter, unlike positional arguments
+#                     Python knows the names of the arguments that our function receives
+
+def hello(firstName, lastName):
+    print("hello " + firstName + " " + lastName)
+
+hello(lastName="Hasan", firstName="Tauhid")
+
+
+
