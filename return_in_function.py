7e1e0e2dec6b329d9b009a371300260e0955c013
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-02-12 09:01:34 +0300

    understanding return statement in python

---
+++ return_in_function.py
@@ -0,0 +1,10 @@
+# return statement = Functions send Python values/objects back to the caller
+#                    These values/objects are known as the function's return value
+
+def calculateAdd (number1, number2):
+    return number1 + number2
+
+x = calculateAdd(5, 7)
+
+print(x)
+
